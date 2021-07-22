eb1145a1839b236e9ec9b538d9ffab249e74d888
Estigma9 <esduro@gmail.com>
2021-07-21 19:45:51 -0500

    VUE cdn componentes

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Task Board with VUE COMPONENT F-432',
+        sesion: 5,
+        profesores:{
+            'nombre': 'Esteban',
+            'ciclo': '3'
+        },
+        tareas: [],
+        nuevaTarea: '',
+        checked: false,
+    },    
+})
